e14b9476fa4a5f7ff35238a875223b6a7c7d53b2
James McCormac <jmccormac001@gmail.com>
2018-09-26 22:50:44 +0100

    adds more timezones

--- bots/eve_time.py
+++ bots/eve_time.py
@@ -11,6 +11,11 @@ from discord.ext import commands
 Client = discord.Client()
 client = commands.Bot(command_prefix="!")
 
+options = ['samoa', 'hawaii', 'marquesas', 'alaska', 'pacific', 'mountain',
+           'central', 'eastern', 'atlantic', 'saopaulo', 'azores', 'gb', 'paris',
+           'cairo', 'moscow', 'dubai', 'karachi', 'dhaka', 'bangkok', 'hongkong',
+           'tokyo', 'sydney', 'noumea', 'auckland']
+
 def parse_evetime(evetime, error_count, return_message):
     """
     Parse the incoming EVE time
@@ -39,26 +44,80 @@ def parse_timezone(tz, error_count, return_message):
     Parse the timezone to convert to
     """
     # handle timezones here
-    if tz.lower().startswith('pacific'):
+    if tz.lower().startswith('samoa'):
+        print('Converting EVE to Pacific/Samoa')
+        TZ = 'Pacific/Samoa'
+    elif tz.lower().startswith('hawaii'):
+        print('Converting EVE to US/Hawaii')
+        TZ = 'US/Hawaii'
+    elif tz.lower().startswith('marquesas'):
+        print('Converting EVE to Pacific/Marquesas')
+        TZ = 'Pacific/Marquesas'
+    elif tz.lower().startswith('alaska'):
+        print('Converting EVE to US/Alaska')
+        TZ = 'US/Alaska'
+    elif tz.lower().startswith('pacific'):
         print('Converting EVE to US/Pacific')
         TZ = 'US/Pacific'
+    elif tz.lower().startswith('mountain'):
+        print('Converting EVE to US/Mountain')
+        TZ = 'US/Mountain'
     elif tz.lower().startswith('central'):
         print('Converting EVE to US/Central')
         TZ = 'US/Central'
     elif tz.lower().startswith('eastern'):
         print('Converting EVE to US/Eastern')
         TZ = 'US/Eastern'
-    elif tz.lower().startswith('gmt'):
-        print('Converting EVE to GMT')
-        TZ = 'GMT'
-    elif tz.lower().startswith('cet'):
-        print('Converting EVE to CET')
+    elif tz.lower().startswith('atlantic'):
+        print('Converting EVE to Canada/Atlantic')
+        TZ = 'Canada/Atlantic'
+    elif tz.lower().startswith('saopaulo'):
+        print('Converting EVE to America/Sao_Paulo')
+        TZ = 'America/Sao_Paulo'
+    elif tz.lower().startswith('azores'):
+        print('Converting EVE to Atlantic/Azores')
+        TZ = 'Atlantic/Azores'
+    elif tz.lower().startswith('gb'):
+        print('Converting EVE to GB')
+        TZ = 'GB'
+    elif tz.lower().startswith('paris'):
+        print('Converting EVE to Europe/Paris')
         TZ = 'CET'
-    elif tz.lower().startswith('nz'):
+    elif tz.lower().startswith('cairo'):
+        print('Converting EVE to Africa/Cairo')
+        TZ = 'Africa/Cairo'
+    elif tz.lower().startswith('moscow'):
+        print('Converting EVE to Europe/Moscow')
+        TZ = 'Europe/Moscow'
+    elif tz.lower().startswith('dubai'):
+        print('Converting EVE to Asia/Dubai')
+        TZ = 'Asia/Dubai'
+    elif tz.lower().startswith('karachi'):
+        print('Converting EVE to Asia/Karachi')
+        TZ = 'Asia/Karachi'
+    elif tz.lower().startswith('dhaka'):
+        print('Converting EVE to Asia/Dhaka')
+        TZ = 'Asia/Dhaka'
+    elif tz.lower().startswith('bangkok'):
+        print('Converting EVE to Asia/Bangkok')
+        TZ = 'Asia/Bangkok'
+    elif tz.lower().startswith('hongkong'):
+        print('Converting EVE to Hongkong')
+        TZ = 'Hongkong'
+    elif tz.lower().startswith('tokyo'):
+        print('Converting EVE to Asia/Tokyo')
+        TZ = 'Asia/Tokyo'
+    elif tz.lower().startswith('sydney'):
+        print('Converting EVE to Australia/Sydney')
+        TZ = 'Australia/Sydney'
+    elif tz.lower().startswith('noumea'):
+        print('Converting EVE to Pacific/Noumea')
+        TZ = 'Pacific/Noumea'
+    elif tz.lower().startswith('auckland'):
         print('Converting EVE to Pacifc/Auckland')
         TZ = 'Pacific/Auckland'
     else:
-        return_message += "Timezone not supported\n"
+        return_message += "Timezone not supported\nSupported timezones: {}".format(', '.join(options))
         error_count += 1
         TZ = None
     return TZ, error_count, return_message
@@ -86,7 +145,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     """
-    Hand incoming messages and convert time requests
+    Handle incoming messages and convert time requests
     """
     sp = message.content.split()
     return_message = ""
@@ -109,7 +168,6 @@ async def on_message(message):
             # once everything above is fine, do the actual conversion, or report on errors
             if error_count == 0:
                 return_message = evetime.astimezone(timezone(TZ)).strftime('%H:%M')
-
         await client.send_message(message.channel, return_message)
 
 client.run('NDkzODgwODM5NzM5MDE1MTY4.Dorbyg.P_uGDGr4YT0CFTqtLg2LRiA-yn4')
