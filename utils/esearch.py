84840fae9140e333851e674096d5f93fa6447a9f
James McCormac <jmccormac001@gmail.com>
2018-11-21 00:05:04 +0000

    adds outputing to file

--- utils/esearch.py
+++ utils/esearch.py
@@ -3,8 +3,14 @@ Search my Eve chat logs for a particular string
 """
 import argparse as ap
 import os
+import platform
 
 CHAT_LOGS_DIR = '/Users/jmcc/Documents/EVE/logs/Chatlogs/'
+
+if 'win' in platform.system().lower():
+    line_ending = '\r\n'
+else:
+    line_ending = '\n'
 
 def arg_parse():
     """
@@ -17,15 +23,16 @@ def arg_parse():
 
 if __name__ == "__main__":
     args = arg_parse()
-    os.chdir(CHAT_LOGS_DIR)
-    chatlogs = [lf for lf in sorted(os.listdir('.')) if lf.endswith('.txt')]
-    for chatlog in chatlogs:
-        hits = []
-        lines = open(chatlog, 'r', encoding='utf-16').readlines()
-        for line in lines:
-            if args.str_to_find in line:
-                hits.append(line)
-        if hits:
-            print('\n{}'.format(chatlog))
-            for hit in hits:
-                print('\t{}'.format(hit.rstrip()))
+    with open('results.txt', 'w') as of:
+        os.chdir(CHAT_LOGS_DIR)
+        chatlogs = [lf for lf in sorted(os.listdir('.')) if lf.endswith('.txt')]
+        for chatlog in chatlogs:
+            hits = []
+            lines = open(chatlog, 'r', encoding='utf-16').readlines()
+            for line in lines:
+                if args.str_to_find in line:
+                    hits.append(line)
+            if hits:
+                of.write('{}{}{}'.format(line_ending, chatlog, line_ending))
+                for hit in hits:
+                    of.write('{}{}'.format(hit.rstrip(), line_ending))
